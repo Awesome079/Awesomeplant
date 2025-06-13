df7fe8b26ebb61faba1efda55ce3dd3f5aa541b3
Awesome079 <kingawesome457@gmail.com>
2025-06-13 21:39:49 +0100

    made some change in my app.js and home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-
-import { BrowserRouter as Router, Routes, Route, useLocation,} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Home from './components/Home';
 import ProductPage from './components/ProductPage';
 import Navbar from './components/Navbar';
@@ -7,27 +6,20 @@ import './App.css';
 import ShoppingPage from "./components/ShoppingPage";
 import CartPage from "./components/CartPage";
 
-
 function App() {
-  
   function Layout() {
     const location = useLocation();
-   
-    const hideNavbar = ['/Home', '/CartPage'].includes(location.pathname);
-    
+    const hideNavbar = ['/', '/CartPage'].includes(location.pathname);
+
     return (
       <>
         {!hideNavbar && <Navbar />}
-        
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/ProductPage" element={<ProductPage />} />
-            <Route path="/ShoppingPage" element={<ShoppingPage />} />
-            <Route path="/CartPage" element={<CartPage />} />
-          </Routes>
-        
-      
-
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/ProductPage" element={<ProductPage />} />
+          <Route path="/ShoppingPage" element={<ShoppingPage />} />
+          <Route path="/CartPage" element={<CartPage />} />
+        </Routes>
       </>
     );
   }
